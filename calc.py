5e8070741c6402458433f9f6dd3f151a738fe2e6
Konrad Jankowski <konrad199907@gmail.com>
2025-04-24 18:02:45 +0200

    final basic exercise

--- calc.py
+++ calc.py
@@ -1,7 +1,28 @@
 import logging
 
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s', filename="D:\Konrad\Desktop\codding\kodilla\4_dzial\calc_4.4\logfile.log")
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s')
 
-logging.info("""\nPodaj działanie, posługując się odpowiednią liczbą:
+print("""Podaj działanie, posługując się odpowiednią liczbą:
 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:""")
-operation = logging.info(input())
+operation = int(input())
+if operation > 4:
+    logging.info("Nie ma takiego działania")
+first_no = input("Podaj liczbę 1.: ")
+second_no = input("Podaj liczbę 2.: ")
+if operation == 1:
+    logging.info(f"Dodaję {first_no} i {second_no}")
+    sum = first_no + second_no
+    print(f"Wynik to: {sum}")
+elif operation == 2:
+    logging.info(f"Odejmuję {second_no} od {first_no}")
+    sum = first_no - second_no
+    print(f"Wynik to: {sum}")
+elif operation == 3:
+    logging.info(f"Możę {first_no} i {second_no}")
+    sum = first_no * second_no
+    print(f"Wynik to: {sum}")
+elif operation == 4:
+    logging.info(f"Dzielę {first_no} przez {second_no}")
+    sum = first_no / second_no
+    print(f"Wynik to: {sum}")
+
