255bd70a72be33986f514974300804f279d8e4f1
Konrad Jankowski <konrad199907@gmail.com>
2025-04-24 17:37:39 +0200

    begin

--- calc.py
+++ calc.py
@@ -1,2 +1,7 @@
 import logging
 
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s', filename="D:\Konrad\Desktop\codding\kodilla\4_dzial\calc_4.4\logfile.log")
+
+logging.info("""\nPodaj działanie, posługując się odpowiednią liczbą:
+1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:""")
+operation = logging.info(input())
